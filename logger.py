375af242d3e63995d790f469aa08710026a0e0d3
Alex Belyaev <t433621@yandex.ru>
2022-10-21 05:56:45 +0300

    Description change

--- logger.py
+++ logger.py
@@ -1,18 +1,20 @@
 # Автор Беляев Александр
 # Модуль для логирования контактов
-from data_in import get_info as gi
+# Данный модуль записывает данные в файлыPhonebook.csv иPhonebook.txt поля как в модуле Дмитрия data_in.
+from data_in import input_data as id
 
-info = gi()
+info = id()
 
 
 def writing_scv():
     file = 'Phonebook.csv'
     with open(file, 'a', encoding='utf-8') as data:
-        data.write(f'{info[0]};{info[1]};{info[2]};{info[3]}\n')
+        data.write(
+            f'{info[0]};{info[1]};{info[2]};{info[3]};{info[4]};{info[5]};{info[6]};\n')
 
 
 def writing_txt():
     file = 'Phonebook.txt'
     with open(file, 'a', encoding='utf-8') as data:
         data.write(
-            f'Фамилия: {info[0]}\n\nИмя: {info[1]}\n\nНомер телефона: {info[2]}\n\nОписание: {info[3]}\n\n\n')
+            f'Фамилия:{info[0]}\n\n Имя:{info[1]}\n\n Jnxtcndj:{info[2]}\n\n Дата рождения:{info[3]}\n\nНомер телефона: {info[4]}\n\n Адрес: {info[5]}\n\nПримичание: {info[6]}\n\n\n')
